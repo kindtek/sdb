46bd6ec7ded78276781241ad86c4072c99a3cd14
kindtek <n8@kindtek.io>
2022-11-27 01:07:39 -0800

    debug

--- build-sdb.sh
+++ build-sdb.sh
@@ -7,11 +7,9 @@ apk add openrc --no-cache
 rc-update add docker
 rc-update add containerd
 rc-service docker start
-chmod +x /build/solana/sdk/docker-solana/build.sh
-chmod +x /build/yubico-net-sdk/Yubico.NativeShims/build-ubuntu.sh
+
 git submodule update --init --recursive /build
-sh /build/solana/sdk/docker-solana/build.sh
-sh /build/yubico-net-sdk/Yubico.NativeShims/build-ubuntu.sh
+
 
 # systemctl enable docker.service
 # systemctl enable containerd.service
