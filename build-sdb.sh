1a55cab1d40e2065d899f4d13b1fbbc1a362c4dd
kindtek <n8@kindtek.io>
2022-11-27 01:00:29 -0800

    reorder build-sdb.sh

--- build-sdb.sh
+++ build-sdb.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 export DOCKER_BUILDKIT=1
 apk update
+apk add --non-cache bash
 apk add --no-cache git
-git submodule update --init --recursive /build
 apk add openrc --no-cache
 rc-update add docker
 rc-update add containerd
 rc-service docker start
 chmod +x ./build/solana/sdk/docker-solana/build.sh
 chmod +x ./build/yubico-net-sdk/Yubico.NativeShims/build-ubuntu.sh
+git submodule update --init --recursive /build
 sh /build/solana/sdk/docker-solana/build.sh
 sh /build/yubico-net-sdk/Yubico.NativeShims/build-ubuntu.sh
 
