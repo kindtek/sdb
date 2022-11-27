965dad432d930bbf82e1e2ebc6bd13a9e59b8a5f
kindtek <n8@kindtek.io>
2022-11-26 18:33:25 -0800

    add systemctl to start docker daemon

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ ARG privileged=true
 ARG rm=true
 ARG cap-add=NET_ADMIN
 ARG cap-add=NET_RAW
+ARG init=true
+ENV DOCKER_TLS_CERTDIR=/certs
 WORKDIR /build
 USER rootless:Rootless
 VOLUME /var/run/docker.sock:/var/run/docker.sock
--- build-sdb.sh
+++ build-sdb.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export DOCKER_BUILDKIT=1
+systemctl enable docker.service
+systemctl enable containerd.service
+systemctl start docker
 
 git clone --branch dev --recurse-submodules -j8 https://github.com/kindtek/sdb.git /build
 /build/solana/sdk/docker-solana/build.sh
