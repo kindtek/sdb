f8c482544cc873dc2825693aeaaa86874871c41e
kindtek <n8@kindtek.io>
2022-11-27 00:55:40 -0800

    remove duplicate

--- build-sdb.sh
+++ build-sdb.sh
@@ -15,7 +15,3 @@ sh /build/yubico-net-sdk/Yubico.NativeShims/build-ubuntu.sh
 # systemctl enable docker.service
 # systemctl enable containerd.service
 # systemctl start docker
-
-
-sh /build/solana/sdk/docker-solana/build.sh
-sh /build/yubico-net-sdk/Yubico.NativeShims/build-ubuntu.sh
