1c0180fb883178a59485361321ea547fbf7cf52c
kindtek <n8@kindtek.io>
2022-11-27 01:03:48 -0800

    remove .

--- build-sdb.sh
+++ build-sdb.sh
@@ -7,8 +7,8 @@ apk add openrc --no-cache
 rc-update add docker
 rc-update add containerd
 rc-service docker start
-chmod +x ./build/solana/sdk/docker-solana/build.sh
-chmod +x ./build/yubico-net-sdk/Yubico.NativeShims/build-ubuntu.sh
+chmod +x /build/solana/sdk/docker-solana/build.sh
+chmod +x /build/yubico-net-sdk/Yubico.NativeShims/build-ubuntu.sh
 git submodule update --init --recursive /build
 sh /build/solana/sdk/docker-solana/build.sh
 sh /build/yubico-net-sdk/Yubico.NativeShims/build-ubuntu.sh
