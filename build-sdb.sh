e0e3db422e59996c7166d28675352f22e95a577e
kindtek <n8@kindtek.io>
2022-11-26 18:08:15 -0800

    add buildkit

--- build-sdb.sh
+++ build-sdb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export DOCKER_BUILDKIT=1
+
 git clone --branch dev --recurse-submodules -j8 https://github.com/kindtek/sdb.git /build
 /build/solana/sdk/docker-solana/build.sh
 /build/yubico-net-sdk/Yubico.NativeShims/docker/Ubuntu/build-ubuntu.sh
