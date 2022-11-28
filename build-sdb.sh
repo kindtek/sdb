2bcd3c61bf68595ed28249f13e787078b15a2649
kindtek <n8@kindtek.io>
2022-11-28 01:13:28 -0800

    more ubuntu adaptations

--- build-sdb.sh
+++ build-sdb.sh
@@ -3,12 +3,10 @@ export DOCKER_BUILDKIT=1
 
 apt-get update
 
-systemctl enable docker.service
-systemctl enable containerd.service
-systemctl start docker
+service start docker
 
-sh CI=true /sdb/solana/sdk/docker-solana/build.sh
-sh /sdb/yubico-net-sdk/Yubico.NativeShims/build-ubuntu.sh
+CI=true ./sdb/solana/sdk/docker-solana/build.sh
+./sdb/yubico-net-sdk/Yubico.NativeShims/build-ubuntu.sh
 # systemctl enable docker.service
 # systemctl enable containerd.service
 # systemctl start docker
