980409756fba316146481f53c6066f19dba888bd
kindtek <n8@kindtek.io>
2022-11-28 00:08:52 -0800

    remove containerd

--- build-sdb.sh
+++ build-sdb.sh
@@ -8,8 +8,6 @@ rc-update add docker
 rc-update add containerd
 rc-service docker start
 
-containerd
-
 sh /sdb/solana/sdk/docker-solana/build.sh
 sh /sdb/yubico-net-sdk/Yubico.NativeShims/build-ubuntu.sh
 # systemctl enable docker.service
